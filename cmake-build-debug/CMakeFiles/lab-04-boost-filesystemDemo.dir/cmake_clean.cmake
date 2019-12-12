0c26cb20f3c188a1f21b2faf6d99889030969717
telsamar <Stolya2000@mail.ru>
2019-12-12 22:25:27 +0300

    READY

---
+++ cmake-build-debug/CMakeFiles/lab-04-boost-filesystemDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab-04-boost-filesystemDemo.dir/sources/main.cpp.o"
+  "lab-04-boost-filesystemDemo.pdb"
+  "lab-04-boost-filesystemDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab-04-boost-filesystemDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sources/main.cpp
@@ -0,0 +1,32 @@
+// Copyright 2019 <Maus74>
+#include <iostream>
+
+#include "BrokerResolver.h"
+
+int main(int argc, char *argv[])
+{
+    boost::filesystem::path path = "";
+    if (argc > 1) {
+        path = argv[1];
+    }
+
+    BrokerResolver resolver{};
+    resolver.resolve(path);
+
+    std::cout << "FILES: \n";
+    for (const BrokerFile &file : resolver.getFileCollection()) {
+        std::cout << file.directoriesToString() << " " <<
+                  file.filename << std::endl;
+    }
+
+    std::cout << "\nDATA: \n";
+    for (const auto &pair : resolver.getDataCollection()) {
+        std::cout << "broker:" << pair.first.broker
+                  << " account:" << pair.first.account
+                  << " files:" << pair.second.files
+                  << " lastdate:" << pair.second.lastDate
+                  << std::endl;
+    }
+
+    return 0;
+}
